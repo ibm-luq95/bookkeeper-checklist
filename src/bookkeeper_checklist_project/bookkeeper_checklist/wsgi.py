83af531a0bac0ca8a52ed88caf3e99b594c22138
Ibrahim Luqman <ibm.jnk@yandex.com>
2022-07-02 19:43:37 +0300

    first commit

---
+++ src/bookkeeper_checklist_project/bookkeeper_checklist/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for bookkeeper_checklist project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bookkeeper_checklist.settings')
+
+application = get_wsgi_application()

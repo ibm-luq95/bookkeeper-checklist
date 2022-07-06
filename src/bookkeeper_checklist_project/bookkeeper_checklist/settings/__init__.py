30cbf950dcdd0a2f463bf442604e3642a9026918
Ibrahim Luqman <ibm.jnk@yandex.com>
2022-07-06 20:17:00 +0300

    feat: create settings paackage

---
+++ src/bookkeeper_checklist_project/bookkeeper_checklist/settings/__init__.py
@@ -0,0 +1,18 @@
+import logging
+import os
+
+from dotenv import load_dotenv
+
+log = logging.getLogger(__name__)
+
+load_dotenv()  # load all environment variables from .env.
+
+
+environment = os.environ.get("STAGE_ENVIRONMENT")
+log.info(f' Environment is: ({environment})')
+if environment == "DEV":
+    from .dev import *
+elif environment == "PRODUCTION":
+    from .production import *
+elif environment == "TEST":
+    from .test import *

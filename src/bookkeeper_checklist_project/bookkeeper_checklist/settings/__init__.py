b7c52a49f621c478f84eca6e4a1592dd89dfd52e
Ibrahim Luqman <ibm.jnk@yandex.com>
2022-09-08 21:01:43 +0300

    fix: fix server .env bug

--- src/bookkeeper_checklist_project/bookkeeper_checklist/settings/__init__.py
+++ src/bookkeeper_checklist_project/bookkeeper_checklist/settings/__init__.py
@@ -5,10 +5,11 @@ from dotenv import load_dotenv
 
 log = logging.getLogger(__name__)
 
-load_dotenv()  # load all environment variables from .env.
-
+dotenv_path = os.path.join("/home/ibrahim/bookkeeper-checklist/src", ".env")
+load_dotenv(dotenv_path=dotenv_path)  # load all environment variables from .env.
 environment = os.environ.get("STAGE_ENVIRONMENT")
 log.info(f" Environment is: ({environment})")
+
 if environment == "DEV":
     from .dev import *
 elif environment == "PRODUCTION":
@@ -17,3 +18,5 @@ elif environment == "TEST":
     from .test import *
 elif environment == "LOCAL":
     from .local import *
+elif environment == "DEV_SERVER":
+    from .dev_server import *

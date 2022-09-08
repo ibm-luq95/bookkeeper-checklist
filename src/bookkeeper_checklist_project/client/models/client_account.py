622cee6afdf76f2b8aad856472143a10699edf14
Ibrahim Luqman <ibm.jnk@yandex.com>
2022-09-08 18:23:58 +0300

    style: format the code

--- src/bookkeeper_checklist_project/client/models/client_account.py
+++ src/bookkeeper_checklist_project/client/models/client_account.py
@@ -8,6 +8,7 @@ class ClientAccount(BaseModelMixin):
     account_email = models.EmailField(_("account_email"), max_length=50, null=True)
     account_url = models.URLField(_("account_url"), max_length=50, null=True)
     account_username = models.CharField(_("account_username"), max_length=30, null=True)
-    account_password = models.CharField(_("account_password"), max_length=250, null=True)
+    account_password = models.CharField(
+        _("account_password"), max_length=250, null=True
+    )
     last_modified_date = models.DateTimeField(_("last_modified_date"), auto_now=True)
-    

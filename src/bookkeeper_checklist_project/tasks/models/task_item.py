65f42863930e367577eba43eb898029610269a88
Ibrahim Luqman <ibm.jnk@yandex.com>
2022-09-25 15:09:12 +0300

    feat: add hints to task item

--- src/bookkeeper_checklist_project/tasks/models/task_item.py
+++ src/bookkeeper_checklist_project/tasks/models/task_item.py
@@ -6,3 +6,4 @@ from django.utils.translation import gettext as _
 class TaskItem(BaseModelMixin):
     title = models.CharField(_("title"), max_length=80, null=True)
     is_completed = models.BooleanField(_("is_completed"), default=False)
+    hints = models.CharField(_("hints"), max_length=50, null=True, blank=True)

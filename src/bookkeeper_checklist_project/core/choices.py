e3155ab49a65aa798b6852cf3395a9f81dae24c7
Ibrahim Luqman <ibm.jnk@yandex.com>
2022-09-26 17:53:06 +0300

    feat: add new TextChoices

--- src/bookkeeper_checklist_project/core/choices.py
+++ src/bookkeeper_checklist_project/core/choices.py
@@ -37,6 +37,17 @@ class ServiceNameEnum(models.TextChoices):
     SHARED_ACCOUNTS = "shared_accounts", _("Shared Accounts")
 
 
+class BaseStatusTypesEnum(models.TextChoices):
+    NO_TYPE = "no_type", _("No Type")
+    RECURRING = "recurring", _("Recurring")
+    WEEKLY = "weekly", _("Weekly")
+    MONTHLY = "monthly", _("Monthly")
+    QUARTERLY = "quarterly", _("Quarterly")
+    YEARLY = "yearly", _("Yearly")
+    ONE_TIME = "one_time", _("One Time")
+    URGENT = "urgent", _("Urgent")
+
+
 class TaskTypeEnum(models.TextChoices):
     NO_TYPE = "no_type", _("No Type")
     RECURRING = "recurring", _("Recurring")
@@ -48,9 +59,20 @@ class TaskTypeEnum(models.TextChoices):
     URGENT = "urgent", _("Urgent")
 
 
-class TaskStatusEnum(models.TextChoices):
+class JobTypeEnum(models.TextChoices):
+    NO_TYPE = "no_type", _("No Type")
+    RECURRING = "recurring", _("Recurring")
+    WEEKLY = "weekly", _("Weekly")
+    MONTHLY = "monthly", _("Monthly")
+    QUARTERLY = "quarterly", _("Quarterly")
+    YEARLY = "yearly", _("Yearly")
+    ONE_TIME = "one_time", _("One Time")
+    URGENT = "urgent", _("Urgent")
+
+
+class JobStatusEnum(models.TextChoices):
     NOT_STARTED = "not_started", _("Not Started")
-    IN_PROGRESS = "in_progress", _("In progress")
+    IN_PROGRESS = "in_progress", _("In Progress")
     PAST_DUE = "past_due", _("Past Due")
     COMPLETE = "complete", _("Complete")
     ARCHIVE = "archive", _("Archive")

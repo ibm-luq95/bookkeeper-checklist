45763080b714313f4d6d0d3c2ff18f4c60905c3e
Ibrahim Luqman <ibm.jnk@yandex.com>
2022-09-26 17:52:48 +0300

    fix: delete old tasks files

--- src/bookkeeper_checklist_project/tasks/models/task_item.py
+++
@@ -1,9 +0,0 @@
-from core.models import BaseModelMixin
-from django.db import models
-from django.utils.translation import gettext as _
-
-
-class TaskItem(BaseModelMixin):
-    title = models.CharField(_("title"), max_length=80, null=True)
-    is_completed = models.BooleanField(_("is_completed"), default=False)
-    hints = models.CharField(_("hints"), max_length=50, null=True, blank=True)

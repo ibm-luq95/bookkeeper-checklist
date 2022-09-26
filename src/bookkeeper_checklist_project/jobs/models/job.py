6805c69b16f62a9242d162e2bf1ebf318761ebc5
Ibrahim Luqman <ibm.jnk@yandex.com>
2022-09-26 17:55:32 +0300

    feat: create new app, jobs app

---
+++ src/bookkeeper_checklist_project/jobs/models/job.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-#
+from core.models import BaseModelMixin
+from django.db import models
+from core.choices import JobStatusEnum, JobTypeEnum
+from django.utils.translation import gettext as _
+from client.models import Client
+from task.models import Task
+from .help_messages import JOB_HELP_MESSAGES
+
+
+class Job(BaseModelMixin):
+    """This is the job for every bookkeeper and assistant
+
+    Args:
+        BaseModelMixin (models.Model): Django model base mixin
+    """
+
+    title = models.CharField(
+        _("title"), max_length=70, null=False, help_text=JOB_HELP_MESSAGES.get("title")
+    )
+    note = models.TextField(
+        _("note"), null=True, help_text=JOB_HELP_MESSAGES.get("note")
+    )
+    job_type = models.CharField(
+        _("job_type"),
+        choices=JobTypeEnum.choices,
+        default=JobTypeEnum.NO_TYPE,
+        max_length=20,
+        help_text=JOB_HELP_MESSAGES.get("job_type"),
+    )
+    job_type = models.CharField(
+        _("job_type"),
+        max_length=20,
+        choices=JobStatusEnum.choices,
+        default=JobStatusEnum.NOT_STARTED,
+        help_text=JOB_HELP_MESSAGES.get("job_type"),
+    )
+    client = models.ForeignKey(
+        to=Client,
+        on_delete=models.PROTECT,
+        null=True,
+        help_text=JOB_HELP_MESSAGES.get("client"),
+    )
+    tasks = models.ManyToManyField(to=Task, help_text=JOB_HELP_MESSAGES.get("tasks"))

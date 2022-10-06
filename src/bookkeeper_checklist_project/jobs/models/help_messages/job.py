98d955958a33de0a7113bfdcc3639fffc7f0f924
Ibrahim Luqman <ibm.jnk@yandex.com>
2022-10-06 20:16:43 +0300

    fix: fix some help test

--- src/bookkeeper_checklist_project/jobs/models/help_messages/job.py
+++ src/bookkeeper_checklist_project/jobs/models/help_messages/job.py
@@ -2,10 +2,11 @@
 from django.utils.translation import gettext as _
 
 JOB_HELP_MESSAGES = {
-    "title": _("The Job Title"),
+    "bookkeeper": _("Bookkeeper of the job"),
+    "title": _("Job Title"),
     "note": _("Note(s) for this job (optional)"),
     "job_type": _("Type of this job, Recurring, Weekly, Urgent,..etc"),
     "status": _("Status of this job, Not Started, In Progress, Done,..etc"),
-    "client": _("The client this job belong to"),
+    "client": _("Client this job belong to"),
     "tasks": _("Tasks of this job")
 }

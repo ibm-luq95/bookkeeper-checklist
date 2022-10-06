f0a511382245fc0e21906158ba4d4a790da98592
Ibrahim Luqman <ibm.jnk@yandex.com>
2022-10-06 20:17:05 +0300

    feat: add details url

--- src/bookkeeper_checklist_project/manager/urls/bookkeeper.py
+++ src/bookkeeper_checklist_project/manager/urls/bookkeeper.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from manager.views import BookkeepersListView
+from manager.views import BookkeepersListView, BookkeepersDetailsView
 
 urlpatterns = [
     path("", BookkeepersListView.as_view(), name="bookkeeper-list"),
+    path("<slug:slug>", BookkeepersDetailsView.as_view(), name="bookkeeper-details"),
 ]

324d26847141cf4d458ee31a78de4603571d0f3c
Ibrahim Luqman <ibm.jnk@yandex.com>
2022-10-08 14:05:51 +0300

    feat: add faker data to account and services

--- src/bookkeeper_checklist_project/bookkeeper/views/clients.py
+++ src/bookkeeper_checklist_project/bookkeeper/views/clients.py
@@ -24,4 +24,14 @@ class ClientsDetailsView(TemplateView):
         # Call the base implementation first to get a context
         context = super().get_context_data(**kwargs)
         context.setdefault("title", "Client Details")
+        acc_and_services = list()
+        fake = faker.Faker()
+        for _ in range(35):
+            acc_and_services.append({
+                "id": fake.pyint(),
+                "url": fake.url(),
+                "username": fake.user_name(),
+                "password": fake.password()
+            })
+        context.setdefault("fake_data", acc_and_services)
         return context

4f4d1152be8bce0af21d4ab2f86f178d9e4be49f
Ibrahim Luqman <ibm.jnk@yandex.com>
2022-10-08 18:57:34 +0300

    feat: add login required and authorization mixin

--- src/bookkeeper_checklist_project/assistant/views/bookkeepers.py
+++ src/bookkeeper_checklist_project/assistant/views/bookkeepers.py
@@ -1,8 +1,12 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 from django.views.generic.base import TemplateView
 
+from .mixins import AssistantAccessMixin
 
-class BookkeepersListView(TemplateView):
+class BookkeepersListView(LoginRequiredMixin, AssistantAccessMixin, TemplateView):
     template_name = "assistant/bookkeepers/list.html"
+    login_url = reverse_lazy("users:login")
 
     def get_context_data(self, **kwargs):
         # Call the base implementation first to get a context
--- src/bookkeeper_checklist_project/assistant/views/dashboard.py
+++ src/bookkeeper_checklist_project/assistant/views/dashboard.py
@@ -1,8 +1,13 @@
 from django.views.generic.base import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+
+from .mixins import AssistantAccessMixin
 import faker
 
-class DashboardView(TemplateView):
+class DashboardView(LoginRequiredMixin, AssistantAccessMixin, TemplateView):
     template_name = "assistant/dashboard/dashboard.html"
+    login_url = reverse_lazy("users:login")
 
     def get_context_data(self, **kwargs):
         # Call the base implementation first to get a context

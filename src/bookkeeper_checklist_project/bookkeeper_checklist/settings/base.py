05f67f5c67bfe1ac0806e043e073a26a9c354621
Ibrahim Luqman <ibm.jnk@yandex.com>
2022-10-09 16:25:44 +0300

    fix: fix static path

--- src/bookkeeper_checklist_project/bookkeeper_checklist/settings/base.py
+++ src/bookkeeper_checklist_project/bookkeeper_checklist/settings/base.py
@@ -141,7 +141,9 @@ STATIC_URL = "static/"
 
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+# STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATIC_ROOT = BASE_DIR / "static"
 
 MEDIA_ROOT = BASE_DIR / "media"
 

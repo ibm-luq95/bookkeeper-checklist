54e408b0b559cf3fd0acea59abc28f5a9a6ecf0d
Ibrahim Luqman <ibm.jnk@yandex.com>
2022-10-23 15:20:02 +0300

    fix: create base admin model mixin to all admin sites

--- src/bookkeeper_checklist_project/assistant/admin.py
+++ src/bookkeeper_checklist_project/assistant/admin.py
@@ -1,7 +1,9 @@
+from core.admin import BaseAdminModelMixin
 from django.contrib import admin
+
 from .models import Assistant
 
 
 @admin.register(Assistant)
-class AssistantAdmin(admin.ModelAdmin):
+class AssistantAdmin(BaseAdminModelMixin):
     pass

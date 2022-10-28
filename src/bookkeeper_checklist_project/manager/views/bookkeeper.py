c5f4f047f7056d1b483274e9daea919e454c7031
Ibrahim Luqman <ibm.jnk@yandex.com>
2022-10-28 21:31:45 +0300

    fix: add job form the detail page context

--- src/bookkeeper_checklist_project/manager/views/bookkeeper.py
+++ src/bookkeeper_checklist_project/manager/views/bookkeeper.py
@@ -2,6 +2,7 @@ from bookkeeper.models import Bookkeeper
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, DetailView
 from django.urls import reverse_lazy
+from jobs.forms import JobForm
 
 from .mixins import ManagerAccessMixin
 
@@ -10,7 +11,7 @@ class BookkeepersListView(LoginRequiredMixin, ManagerAccessMixin, ListView):
     login_url = reverse_lazy("users:login")
     template_name: str = "manager/bookkeeper/list.html"
     model = Bookkeeper
-    paginate_by: int = 10
+    # paginate_by: int = 10
 
     def get_context_data(self, **kwargs):
         # Call the base implementation first to get a context
@@ -28,4 +29,6 @@ class BookkeepersDetailsView(LoginRequiredMixin, ManagerAccessMixin, DetailView)
         context = super().get_context_data(**kwargs)
         bookkeeper_fullname = self.get_object().user.fullname
         context["title"] = f"{bookkeeper_fullname}"
+        context["jobs_form"] = JobForm(bookkeeper=self.get_object())
+        # print(context["jobs_form"].data)
         return context

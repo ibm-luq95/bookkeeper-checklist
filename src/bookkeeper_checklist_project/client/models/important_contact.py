06d21dec29152d5377980b2dc6ddb33b3ad36dda
Ibrahim Luqman <ibm.jnk@yandex.com>
2022-11-07 17:15:54 +0300

    refact: refactor model
    refactor important contact model

--- src/bookkeeper_checklist_project/client/models/important_contact.py
+++ src/bookkeeper_checklist_project/client/models/important_contact.py
@@ -1,6 +1,7 @@
-from core.models import BaseModelMixin
 from django.db import models
 from django.utils.translation import gettext as _
+
+from core.models import BaseModelMixin
 
 
 class ImportantContact(BaseModelMixin):
@@ -10,6 +11,18 @@ class ImportantContact(BaseModelMixin):
         BaseModelMixin (models.Model): Django base model mixin
     """
 
-    contact_fields = models.JSONField(
-        _("contact_fields"), default=dict, null=True, blank=True
+    company_name = models.CharField(
+        _("company name"), max_length=60, null=True, blank=True
     )
+    description = models.TextField(_("description"), null=True, blank=True)
+    first_name = models.CharField(_("first name"), max_length=50, null=True, blank=True)
+    last_name = models.CharField(_("last name"), max_length=50, null=True, blank=True)
+    email = models.EmailField(_("email"), null=True, blank=True)
+    city = models.CharField(_("city"), max_length=50, null=True, blank=True)
+    street_address = models.CharField(
+        _("street address"), max_length=50, null=True, blank=True
+    )
+    state = models.CharField(_("state"), max_length=50, null=True, blank=True)
+    postcode = models.CharField(_("postcode"), max_length=50, null=True, blank=True)
+    phone = models.CharField(_("phone"), max_length=80, null=True, blank=True)
+    website = models.URLField(_("website"), null=True, blank=True)

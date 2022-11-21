cae34b3e7d6b35427428b5621394af8683a787d8
Ibrahim Luqman <ibm.jnk@yandex.com>
2022-11-21 17:34:04 +0300

    fix: fix permission admin model

--- src/bookkeeper_checklist_project/users/admin.py
+++ src/bookkeeper_checklist_project/users/admin.py
@@ -1,16 +1,17 @@
-from core.admin import BaseAdminModelMixin
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import Permission
 
+from core.admin import BaseAdminModelMixin
 from .models import CustomUser
 
 
 @admin.register(Permission)
-class PermissionAdmin(BaseAdminModelMixin):
+class PermissionAdmin(admin.ModelAdmin):
     pass
 
 
 @admin.register(CustomUser)
 class CustomUserAdmin(BaseAdminModelMixin):
     list_filter = ("user_type", "created_at", "updated_at", "is_active")
+    # list_display = ("email",)

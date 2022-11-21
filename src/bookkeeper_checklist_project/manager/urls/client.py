68c4e315deba5ccb7fd017279341fff159d6da93
Ibrahim Luqman <ibm.jnk@yandex.com>
2022-11-21 17:53:53 +0300

    feat: new client urls

--- src/bookkeeper_checklist_project/manager/urls/client.py
+++ src/bookkeeper_checklist_project/manager/urls/client.py
@@ -1,10 +1,19 @@
 # -*- coding: utf-8 -*-#
 from django.urls import path, include
-from manager.views import ClientListView, ClientCreateView
+from manager.views import (
+    ClientListView,
+    ClientCreateView,
+    ClientDetailView,
+    ClientUpdateView,
+    ClientDeleteView
+)
 
 app_name = "client"
 
 urlpatterns = [
     path("", ClientListView.as_view(), name="list"),
     path("create", ClientCreateView.as_view(), name="create"),
+    path("<uuid:pk>", ClientDetailView.as_view(), name="details"),
+    path("update/<uuid:pk>", ClientUpdateView.as_view(), name="update"),
+    path("delete/<uuid:pk>", ClientDeleteView.as_view(), name="delete"),
 ]

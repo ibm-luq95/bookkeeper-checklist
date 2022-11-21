3fcb235c7ce585d625350e52e77ee1c9213486df
Ibrahim Luqman <ibm.jnk@yandex.com>
2022-11-21 17:38:47 +0300

    last static js files

--- src/bookkeeper_checklist_project/static/js/utils/model-box.js
+++ src/bookkeeper_checklist_project/static/js/utils/model-box.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const showMicroModal = (modalId, onCloseCallback) => {
+const showMicroModal = (modalId, onCloseCallback, onOpenCallBack) => {
   MicroModal.show(modalId, {
     disableScroll: false,
     onClose: (modal) => {
@@ -9,8 +9,49 @@ const showMicroModal = (modalId, onCloseCallback) => {
         onCloseCallback();
       }
     },
+    onShow: (modal) => {
+      //   console.info(`${modal.id} is hidden`);
+      if (typeof onOpenCallBack == "function") {
+        onOpenCallBack();
+      }
+    },
+
     disableFocus: false,
   });
 };
 
-export { showMicroModal };
+class MicroModalHandler {
+  constructor(modalId, callBacks) {
+    this.modalId = modalId;
+    if (typeof callBacks["onCloseCallback"] == "function") {
+      this.onCloseCallback = callBacks["onCloseCallback"];
+    }
+    if (typeof callBacks["onOpenCallBack"] == "function") {
+      this.onOpenCallBack = callBacks["onOpenCallBack"];
+    }
+
+    this.fireModal();
+  }
+
+  fireModal() {
+    MicroModal.show(this.modalId, {
+      disableScroll: false,
+      onClose: (modal) => {
+        //   console.info(`${modal.id} is hidden`);
+        if (typeof this.onCloseCallback == "function") {
+          this.onCloseCallback();
+        }
+      },
+      onShow: (modal) => {
+        //   console.info(`${modal.id} is hidden`);
+        if (typeof this.onOpenCallBack == "function") {
+          this.onOpenCallBack();
+        }
+      },
+
+      disableFocus: false,
+    });
+  }
+}
+
+export { showMicroModal, MicroModalHandler };

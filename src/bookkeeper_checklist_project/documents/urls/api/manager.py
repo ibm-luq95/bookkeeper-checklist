4aee3c419e6764dfd0fcdf22ef913bcc4e70ed33
Ibrahim Luqman <ibm.jnk@yandex.com>
2022-11-26 13:10:00 +0300

    new: delete document api url

--- src/bookkeeper_checklist_project/documents/urls/api/manager.py
+++ src/bookkeeper_checklist_project/documents/urls/api/manager.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-#
 from django.urls import path
 
-from documents.views.api import CreateDocumentManagerApiView
+from documents.views.api import CreateDocumentManagerApiView, DeleteDocumentManagerApiView
 
 app_name = "manager_api"
 
@@ -10,5 +10,6 @@ urlpatterns = [
         "create-document",
         CreateDocumentManagerApiView.as_view(),
         name="create-document-manager",
-    )
+    ),
+    path("delete", DeleteDocumentManagerApiView.as_view(), name="delete-document"),
 ]

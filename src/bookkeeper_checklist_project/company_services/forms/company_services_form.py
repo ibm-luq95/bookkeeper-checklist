3b7a42d6282660b97ef2d544fcece84fdd3bee55
Ibrahim Luqman <ibm.jnk@yandex.com>
2022-11-26 13:08:00 +0300

    fix: fix url css class

--- src/bookkeeper_checklist_project/company_services/forms/company_services_form.py
+++ src/bookkeeper_checklist_project/company_services/forms/company_services_form.py
@@ -5,7 +5,7 @@ from core.forms import BaseModelFormMixin
 class CompanyServiceForm(BaseModelFormMixin):
     def __init__(self, client=None, *args, **kwargs):
         super(CompanyServiceForm, self).__init__(*args, **kwargs)
-
+        self.fields["url"].widget.attrs.update({"class": "input", "type": "url"})
         if client is not None:
             self.fields["client"].initial = client
         #     # self.fields["bookkeeper"].widget.attrs.update({"disabled": "disabled"})

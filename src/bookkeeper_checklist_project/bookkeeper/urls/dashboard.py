7f2380fa004f724e169947fb77c4081243b8646b
Ibrahim Luqman <ibm.jnk@yandex.com>
2022-11-28 17:24:52 +0300

    feat: add jobs urls

--- src/bookkeeper_checklist_project/bookkeeper/urls/dashboard.py
+++ src/bookkeeper_checklist_project/bookkeeper/urls/dashboard.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 from bookkeeper.views import DashboardView
 
+# app_name = "dashboard"
 
 urlpatterns = [
     path("", DashboardView.as_view(), name="dashboard"),
     path("clients/", include("bookkeeper.urls.clients"), name="clients-urls"),
+    path("jobs/", include("bookkeeper.urls.jobs"), name="jobs-urls"),
 ]

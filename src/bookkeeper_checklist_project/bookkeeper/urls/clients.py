5fc642f1b22221895bdcd6635c0478093693732b
Ibrahim Luqman <ibm.jnk@yandex.com>
2022-11-28 17:24:38 +0300

    fix: fix client details url

--- src/bookkeeper_checklist_project/bookkeeper/urls/clients.py
+++ src/bookkeeper_checklist_project/bookkeeper/urls/clients.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
 from bookkeeper.views import ClientsListView, ClientsDetailsView
 
+app_name = "client"
 
 urlpatterns = [
-    path("", ClientsListView.as_view(), name="clients-list"),
-    path("<int:pk>", ClientsDetailsView.as_view(), name="clients-details"),
+    path("", ClientsListView.as_view(), name="list"),
+    path("<uuid:pk>", ClientsDetailsView.as_view(), name="details"),
 ]

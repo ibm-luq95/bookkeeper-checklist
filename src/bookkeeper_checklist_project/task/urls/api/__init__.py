59e734f9428ffcf0d5267063ca699cc6db32b823
Ibrahim Luqman <ibm.jnk@yandex.com>
2022-12-01 21:54:07 +0300

    fix: add bookkeeper urls and views

--- src/bookkeeper_checklist_project/task/urls/api/__init__.py
+++ src/bookkeeper_checklist_project/task/urls/api/__init__.py
@@ -4,7 +4,8 @@ from django.urls import path, include
 app_name = "api"
 
 urlpatterns = [
+    path("manager/", include("task.urls.api.manager"), name="manager-task-api-urls"),
     path(
-        "manager/", include("task.urls.api.manager"), name="manager-task-api-urls"
-    )
+        "bookkeeper/", include("task.urls.api.bookkeeper"), name="bookkeeper-task-api-urls"
+    ),
 ]

f960848b82d36635e3757be2651c1d4294bb92cb
Ibrahim Luqman <ibm.jnk@yandex.com>
2022-12-05 15:02:21 +0300

    feat: add serializer constants

--- src/bookkeeper_checklist_project/core/constants/__init__.py
+++ src/bookkeeper_checklist_project/core/constants/__init__.py
@@ -1,3 +1,4 @@
 # -*- coding: utf-8 -*-#
 from .users import BOOKKEEPER_GROUP_NAME, ASSISTANT_GROUP_NAME, MANAGER_GROUP_NAME
 from .admin import ADMIN_FILTER_LIST
+from .serializers import EXCLUDED_FIELDS

c2b9e769da36678084641ffd6fd13b016a1cf188
Ibrahim Luqman <ibm.jnk@yandex.com>
2022-12-05 15:04:05 +0300

    feat: add retrieve, delete, and update views for job

--- src/bookkeeper_checklist_project/jobs/urls/api/manager.py
+++ src/bookkeeper_checklist_project/jobs/urls/api/manager.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-#
 from django.urls import path
 
-from jobs.views.api import CreateJobManagerApiView
+from jobs.views.api import (
+    CreateJobManagerApiView,
+    RetrieveJobManagerApiView,
+    UpdateJobManagerApiView,
+    DeleteJobManagerApiView,
+)
 
 app_name = "manager"
 
@@ -11,4 +16,7 @@ urlpatterns = [
         CreateJobManagerApiView.as_view(),
         name="create",
     ),
+    path("retrieve", RetrieveJobManagerApiView.as_view(), name="retrieve"),
+    path("update", UpdateJobManagerApiView.as_view(), name="update"),
+    path("delete", DeleteJobManagerApiView.as_view(), name="delete"),
 ]

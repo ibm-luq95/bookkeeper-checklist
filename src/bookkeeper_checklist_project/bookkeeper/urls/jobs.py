521593bd017ff617903be07d2e6412bdcb8994d4
Ibrahim Luqman <ibm.jnk@yandex.com>
2022-12-11 15:15:13 +0300

    new: add task details view

--- src/bookkeeper_checklist_project/bookkeeper/urls/jobs.py
+++ src/bookkeeper_checklist_project/bookkeeper/urls/jobs.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-#
 from django.urls import path
-from bookkeeper.views import JobListView
+from bookkeeper.views import JobListView, JobDetailView
 
 app_name = "job"
 
 urlpatterns = [
     path("", JobListView.as_view(), name="list"),
+    path("<uuid:pk>", JobDetailView.as_view(), name="details"),
 ]

a5a56e6ea232d21b8972842500dbf144590171e4
Ibrahim Luqman <ibm.jnk@yandex.com>
2022-12-11 15:18:00 +0300

    fix: validate due date when create new job

--- src/bookkeeper_checklist_project/jobs/serializers/jobs.py
+++ src/bookkeeper_checklist_project/jobs/serializers/jobs.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-
+from django.utils import timezone
 from core.constants import EXCLUDED_FIELDS
 from jobs.models import Job
 from task.serializers import TaskSerializer
@@ -17,6 +17,15 @@ class CreateJobSerializer(serializers.ModelSerializer):
             "is_deleted",
         )
         # depth = 1
+
+    def validate(self, data):
+        """
+        Check that start is before finish.
+        """
+        now = timezone.now().date()
+        if data["due_date"] < now:
+            raise serializers.ValidationError({"due_date": "Due date old!"})
+        return data
 
 
 class JobSerializer(serializers.ModelSerializer):

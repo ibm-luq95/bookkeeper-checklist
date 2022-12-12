2a034c0af4bcfb75a93c25a0600f50570e3b3b39
Ibrahim Luqman <ibm.jnk@yandex.com>
2022-12-12 14:28:59 +0300

    fix: fix tasks list bug, and validate due date when update task

--- src/bookkeeper_checklist_project/bookkeeper/views/tasks.py
+++ src/bookkeeper_checklist_project/bookkeeper/views/tasks.py
@@ -1,18 +1,16 @@
 # -*- coding: utf-8 -*-#
-import traceback
 
-import faker
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.urls import reverse_lazy
 from django.contrib.messages.views import SuccessMessageMixin
-from django.views.generic import ListView, DetailView, UpdateView
-from django.views.generic.base import TemplateView
+from django.core.exceptions import ImproperlyConfigured
+from django.db.models import QuerySet
+from django.urls import reverse_lazy
+from django.views.generic import ListView, UpdateView
+from django.contrib import messages
 
-from client.models import Client
-from core.utils import get_formatted_logger, debugging_print
-from task.models import Task
+from core.utils import get_formatted_logger
 from task.forms import TaskForm
-from jobs.models import Job
+from task.models import Task
 from .mixins import BookkeeperAccessMixin
 
 logger = get_formatted_logger(__file__)
@@ -30,18 +28,46 @@ class TaskListView(LoginRequiredMixin, BookkeeperAccessMixin, ListView):
 
         return context
 
-    # def get_queryset(self):
-    #     try:
-    #         bookkeeper = self.request.user.bookkeeper_related.get()
-    #         jobs = bookkeeper.jobs.all()
-    #         queryset = jobs
-    #         return queryset
-    #     except Exception as ex:
-    #         logger.error(traceback.format_exc())
-    #         raise Exception(str(ex))
+    def get_queryset(self):
+        """
+        Return the list of items for this view.
+        The return value must be an iterable and may be an instance of
+        `QuerySet` in which case `QuerySet` specific behavior will be enabled.
+        """
+        bookkeeper = self.request.user.bookkeeper_related.get()
+        tasks_list = []
+        if self.queryset is not None:
+            queryset = self.queryset
+            if isinstance(queryset, QuerySet):
+                queryset = queryset.all()
+        elif self.model is not None:
+            # queryset = self.model._default_manager.all()  # Default
+            for job in bookkeeper.jobs.all():
+                # debugging_print(f"Job {job.title}")
+                job_tasks = job.tasks.select_related().filter()
+                # debugging_print(job_tasks)
+                if job_tasks:
+                    # debugging_print(job_tasks.count())
+                    for task in job_tasks:
+                        # debugging_print(dir(task.pk))
+                        tasks_list.append(task.pk)
+            queryset = Task.objects.prefetch_related().filter(pk__in=tasks_list)
+        else:
+            # debugging_print("IN ELSE")
+            raise ImproperlyConfigured(
+                "%(cls)s is missing a QuerySet. Define "
+                "%(cls)s.model, %(cls)s.queryset, or override "
+                "%(cls)s.get_queryset()." % {"cls": self.__class__.__name__}
+            )
+        ordering = self.get_ordering()
+        if ordering:
+            if isinstance(ordering, str):
+                ordering = (ordering,)
+            queryset = queryset.order_by(*ordering)
+        return queryset
 
 
-class TaskDetailView(
+class TaskUpdateView(
     LoginRequiredMixin, BookkeeperAccessMixin, SuccessMessageMixin, UpdateView
 ):
     template_name = "bookkeeper/tasks/details.html"
@@ -64,5 +90,15 @@ class TaskDetailView(
         if hasattr(self, "object"):
             kwargs.update({"instance": self.object})
         kwargs.update({"is_disable_job": True})
-        debugging_print(kwargs)
         return kwargs
+
+    def form_valid(self, form):
+        """If the form is valid, save the associated model."""
+        start_date = form.cleaned_data.get("start_date")
+        due_date = form.cleaned_data.get("due_date")
+        if start_date > due_date:
+            messages.error(self.request, "Error while update task!")
+            form.add_error("start_date", "Due date must occur after start date")
+            return super().form_invalid(form)
+        self.object = form.save()
+        return super().form_valid(form)

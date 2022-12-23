571ebd75034bee98cb2a49eef7827545eaf902e7
Ibrahim Luqman <ibm.jnk@yandex.com>
2022-12-23 12:19:15 +0300

    feat: add RetrieveManagerDocumentView

--- src/bookkeeper_checklist_project/documents/views/api/__init__.py
+++ src/bookkeeper_checklist_project/documents/views/api/__init__.py
@@ -1,2 +1,6 @@
 # -*- coding: utf-8 -*-#
-from .manager import CreateDocumentManagerApiView, DeleteDocumentManagerApiView
+from .manager import (
+    CreateDocumentManagerApiView,
+    DeleteDocumentManagerApiView,
+    RetrieveManagerDocumentView,
+)
--- src/bookkeeper_checklist_project/documents/views/api/manager.py
+++ src/bookkeeper_checklist_project/documents/views/api/manager.py
@@ -93,3 +93,37 @@ class DeleteDocumentManagerApiView(APIView):
                 "user_error_msg": "Error while delete document!",
             }
             return Response(response_data, status=status.HTTP_400_BAD_REQUEST)
+
+
+class RetrieveManagerDocumentView(APIView):
+    permission_classes = [permissions.IsAdminUser]
+
+    def post(self, request: Request, *args, **kwargs):
+        serializer = ""
+        try:
+            data = request.data
+            document_id = data.get("documentId")
+            document = Documents.objects.select_related().filter(pk=document_id).first()
+            document_serializer = CreateDocumentSerializer(instance=document)
+            return Response(
+                data={"document": document_serializer.data},
+                status=status.HTTP_201_CREATED,
+            )
+        except APIException as ex:
+            # logger.error("API Exception")
+            logger.error(ex)
+            response_data = {
+                "status": status.HTTP_400_BAD_REQUEST,
+                "user_error_msg": ex.detail,
+                # "user_error_msg": serializer.error_messages,
+            }
+            return Response(response_data, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as ex:
+            debugging_print(ex)
+            logger.error(traceback.format_exc())
+            response_data = {
+                "status": status.HTTP_400_BAD_REQUEST,
+                "error": str(ex),
+                "user_error_msg": "Error while retrieve document!",
+            }
+            return Response(response_data, status=status.HTTP_400_BAD_REQUEST)

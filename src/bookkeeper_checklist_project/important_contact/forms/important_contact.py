b4fe9ed6ea271c87ea358c33b8bd6ab93811f7da
Ibrahim Luqman <ibm.jnk@yandex.com>
2022-12-25 14:38:45 +0300

    fix: several fixes

--- src/bookkeeper_checklist_project/important_contact/forms/important_contact.py
+++ src/bookkeeper_checklist_project/important_contact/forms/important_contact.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-#
+from client.models import Client
 from core.forms import BaseModelFormMixin
 from important_contact.models import ImportantContact
 
@@ -5,7 +7,15 @@ from important_contact.models import ImportantContact
 class ImportantContactForm(BaseModelFormMixin):
     # auto_id = "important_contact_%s"
 
-    def __init__(self, is_readonly=False, is_creating=False, *args, **kwargs):
+    def __init__(
+        self,
+        is_readonly=False,
+        client_pk=None,
+        is_creating=False,
+        remove_client_field=False,
+        *args,
+        **kwargs
+    ):
         super(ImportantContactForm, self).__init__(*args, **kwargs)
         # print(is_creating)
         self.fields["contact_website"].widget.attrs.update(
@@ -14,10 +24,20 @@ class ImportantContactForm(BaseModelFormMixin):
         if is_readonly is True:
             for field in self.fields:
                 self.fields[field].widget.attrs.update({"readonly": "readonly"})
+            self.fields["contact_label"].widget.attrs.update({"class": "readonly-select"})
+
+        # check if the client passed in the arguments
+        if client_pk is not None:
+            self.fields["client"].initial = (
+                Client.objects.select_related().filter(pk=client_pk).first()
+            )
+
+        # remove client input
+        if remove_client_field is True:
+            self.fields.pop("client")
 
     class Meta(BaseModelFormMixin.Meta):
         model = ImportantContact
-        # exclude = ["client", "metadata", "is_deleted"]
         # widgets = {
         #     "account_url": forms.URLInput()
         # }

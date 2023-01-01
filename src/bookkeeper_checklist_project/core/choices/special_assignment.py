2f700e7ccfbb98c11c62c5093ba448e92f12c165
Ibrahim Luqman <ibm.jnk@yandex.com>
2023-01-01 14:22:04 +0300

    new: create SpecialAssignmentStatusEnum

---
+++ src/bookkeeper_checklist_project/core/choices/special_assignment.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-#
+from django.db import models
+from django.utils.translation import gettext as _
+
+
+class SpecialAssignmentStatusEnum(models.TextChoices):
+    NOT_STARTED = "not_started", _("Not Started")
+    IN_PROGRESS = "in_progress", _("In Progress")
+    COMPLETED = "completed", _("Completed")
+    REJECTED = "rejected", _("Rejected")

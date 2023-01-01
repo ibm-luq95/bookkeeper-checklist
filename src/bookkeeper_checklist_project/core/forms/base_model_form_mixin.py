7651ef1cd002d77c919be1b7f73f459001eb0c17
Ibrahim Luqman <ibm.jnk@yandex.com>
2023-01-01 14:18:38 +0300

    feat: extend Html5Mixin to base model form

--- src/bookkeeper_checklist_project/core/forms/base_model_form_mixin.py
+++ src/bookkeeper_checklist_project/core/forms/base_model_form_mixin.py
@@ -1,14 +1,15 @@
-from django import forms
-from core.constants.form import EXCLUDED_FIELDS
 from crispy_forms.helper import FormHelper
+from django import forms
+
+from core.constants.form import EXCLUDED_FIELDS
+from .html5_mixin import Html5Mixin
 
 
-class BaseModelFormMixin(forms.ModelForm):
+class BaseModelFormMixin(Html5Mixin, forms.ModelForm):
     def __init__(self, bookkeeper=None, *args, **kwargs):
         super(BaseModelFormMixin, self).__init__(*args, **kwargs)
         self.helper = FormHelper(self)
         # self.helper.form_horizontal = True
 
     class Meta:
-        # pass
         exclude = EXCLUDED_FIELDS

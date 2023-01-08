985e8040a4313990c4dcf2e894af815409827144
Ibrahim Luqman <ibm.jnk@yandex.com>
2023-01-08 14:48:34 +0300

    fix: new settings for datatable

--- src/bookkeeper_checklist_project/static/js/manager/special_assignment/list.js
+++ src/bookkeeper_checklist_project/static/js/manager/special_assignment/list.js
@@ -1,25 +1,39 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", (readyEvent) => {
-    const clientsTable = $("#specialAssignmentsTable").DataTable({
-        autoWidth: true,
-        processing: true,
-        // "info": false,
-        // "paging": false,
-        // stateSave: true,
-        stateSaveCallback: function (settings, data) {
-          localStorage.setItem("DataTables_" + settings.sInstance, JSON.stringify(data));
-        },
-        stateLoadCallback: function (settings) {
-          return JSON.parse(localStorage.getItem("DataTables_" + settings.sInstance));
-        },
-        fixedHeader: true,
-        responsive: true,
-        dom: "Bfrtip",
-        buttons: [
-          // "copy",
-          "csv",
-          "pdf",
-        ],
-      });
+  const ths = document.querySelectorAll("#specialAssignmentsTable thead tr th");
+  // console.log($('#specialAssignmentsTable thead tr th').length - 1);
+  const clientsTable = $("#specialAssignmentsTable").DataTable({
+    autoWidth: true,
+    processing: true,
+    columnDefs: [
+      { width: "30%", targets: 0 },
+      {targets: ths.length - 1, orderable: false}
+    ],
+    order: [], // this to disable initial order
+    // "info": false,
+    // "paging": false,
+    // stateSave: true,
+    stateSaveCallback: function (settings, data) {
+      localStorage.setItem(
+        "DataTables_" + settings.sInstance,
+        JSON.stringify(data)
+      );
+    },
+    stateLoadCallback: function (settings) {
+      return JSON.parse(
+        localStorage.getItem("DataTables_" + settings.sInstance)
+      );
+    },
+    fixedHeader: true,
+    fixedColumns: true,
+    // scrollCollapse: true,
+    responsive: true,
+    dom: "Bfrtip",
+    buttons: [
+      // "copy",
+      "csv",
+      "pdf",
+    ],
+  });
 });

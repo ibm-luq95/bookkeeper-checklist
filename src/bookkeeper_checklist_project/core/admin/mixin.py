d511c72664dff00e903a7aeea89c859e5aa140c4
Ibrahim Luqman <ibm.jnk@yandex.com>
2023-01-08 14:33:22 +0300

    fix: add readonly_fields, and format the file

--- src/bookkeeper_checklist_project/core/admin/mixin.py
+++ src/bookkeeper_checklist_project/core/admin/mixin.py
@@ -3,5 +3,9 @@ from django.contrib import admin
 
 class BaseAdminModelMixin(admin.ModelAdmin):
     list_filter = ("created_at", "updated_at")
-    exclude = ("metadata", "is_deleted",)
+    exclude = (
+        "metadata",
+        "is_deleted",
+    )
+    readonly_fields = ("deleted_at",)
     # list_display = ["created_at", "is_deleted"]

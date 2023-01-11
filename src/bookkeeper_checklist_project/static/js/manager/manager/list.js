a68d4e9124ed50575e57c369e299a5b84cd9981f
Ibrahim Luqman <ibm.jnk@yandex.com>
2023-01-11 16:53:54 +0300

    manager list js file

---
+++ src/bookkeeper_checklist_project/static/js/manager/manager/list.js
@@ -0,0 +1,8 @@
+"use strict";
+import { DataTableHelper } from "../../utils/datatable-helper.js";
+
+document.addEventListener("DOMContentLoaded", (readyEvent) => {
+  new DataTableHelper({
+    tableID: "managerManagersTable",
+  });
+});

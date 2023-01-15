ed13dc34db17658f42a7a8cf74279ba2d1bae711
Ibrahim Luqman <ibm.jnk@yandex.com>
2023-01-15 14:48:27 +0300

    fix: encrypt saved password when save using api

--- src/bookkeeper_checklist_project/company_services/serializers/create_company_services.py
+++ src/bookkeeper_checklist_project/company_services/serializers/create_company_services.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-#
 from rest_framework import serializers
+
+from company_services.helpers import PasswordHasher
 from company_services.models import CompanyService
 
 
@@ -11,3 +13,7 @@ class CreateCompanyServicesSerializer(serializers.ModelSerializer):
             "is_deleted",
         )
         # depth = 1
+
+    def validate_password(self, value):
+        encrypted = PasswordHasher.encrypt(value)
+        return encrypted

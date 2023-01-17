61c87dd6c8029069a8840578463a731d82ae9958
Ibrahim Luqman <ibm.jnk@yandex.com>
2023-01-17 14:35:24 +0300

    fix: exclude some fields

--- src/bookkeeper_checklist_project/notes/serializers/note.py
+++ src/bookkeeper_checklist_project/notes/serializers/note.py
@@ -9,5 +9,8 @@ class NoteSerializer(serializers.ModelSerializer):
         exclude = (
             "metadata",
             "is_deleted",
+            "created_at",
+            "updated_at",
+            "deleted_at",
         )
         # depth = 1

f17a04d16f2e9ab2d87d5e983ef650cdbd1ad0b3
Ibrahim Luqman <ibm.jnk@yandex.com>
2023-01-24 17:10:16 +0300

    fix: fix ALLOWED_HOSTS

--- src/bookkeeper_checklist_project/bookkeeper_checklist/settings/stage.py
+++ src/bookkeeper_checklist_project/bookkeeper_checklist/settings/stage.py
@@ -1,7 +1,7 @@
 from decouple import config
 from .base import *
 
-ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=str)
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=str).split(", ")
 
 DEBUG = config("DEBUG", cast=bool)
 

9dbca9bf4d31cae4f9ec096b1578eda56a67d113
Ibrahim Luqman <ibm.jnk@yandex.com>
2023-01-24 17:49:38 +0300

    feat: add bookkeeper details view

--- src/bookkeeper_checklist_project/assistant/views/bookkeepers.py
+++ src/bookkeeper_checklist_project/assistant/views/bookkeepers.py
@@ -1,15 +1,33 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic.base import TemplateView
+from django.views.generic import ListView, DetailView
+from bookkeeper.models import Bookkeeper
+from core.utils import get_trans_txt
 
 from .mixins import AssistantAccessMixin
 
-class BookkeepersListView(LoginRequiredMixin, AssistantAccessMixin, TemplateView):
+
+class BookkeepersListView(LoginRequiredMixin, AssistantAccessMixin, ListView):
     template_name = "assistant/bookkeepers/list.html"
     login_url = reverse_lazy("users:login")
+    model = Bookkeeper
 
     def get_context_data(self, **kwargs):
         # Call the base implementation first to get a context
         context = super().get_context_data(**kwargs)
-        context["title"] = "Bookkeepers"
+        context.setdefault("title", get_trans_txt("Bookkeepers"))
+        return context
+
+
+class BookkeepersDetailsView(LoginRequiredMixin, AssistantAccessMixin, DetailView):
+    template_name = "assistant/bookkeepers/details.html"
+    login_url = reverse_lazy("users:login")
+    model = Bookkeeper
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        bookkeeper = self.get_object()
+        context.setdefault("title", bookkeeper.user.fullname)
         return context

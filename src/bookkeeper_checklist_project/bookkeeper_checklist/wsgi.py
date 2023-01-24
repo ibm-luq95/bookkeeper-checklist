3b8568aa02a32815d2b41b5c8e743da7060c1554
Ibrahim Luqman <ibm.jnk@yandex.com>
2023-01-24 15:26:59 +0300

    fix: disable dotenv import

--- src/bookkeeper_checklist_project/bookkeeper_checklist/wsgi.py
+++ src/bookkeeper_checklist_project/bookkeeper_checklist/wsgi.py
@@ -9,11 +9,11 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 
 import os
 import logging
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
 # dotenv_path = os.path.join("/home/ibrahim/bookkeeper-checklist/src", ".env")  # DEV
-dotenv_path = os.path.join("/home/ibrahim/production/bookkeeper-checklist/src", ".env")
-load_dotenv(dotenv_path=dotenv_path)  # load all environment variables from .env.
+# dotenv_path = os.path.join("/home/ibrahim/production/bookkeeper-checklist/src", ".env")
+# load_dotenv(dotenv_path=dotenv_path)  # load all environment variables from .env.
 # load_dotenv()
 # print(os.environ.get("STAGE_ENVIRONMENT"))
 # log = logging.getLogger("my-logger")

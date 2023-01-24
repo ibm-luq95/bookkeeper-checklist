f0bd1e2412e3b0e1be4982090e140a1da14cd45d
Ibrahim Luqman <ibm.jnk@yandex.com>
2023-01-24 14:35:28 +0300

    quick logger

--- src/bookkeeper_checklist_project/bookkeeper_checklist/wsgi.py
+++ src/bookkeeper_checklist_project/bookkeeper_checklist/wsgi.py
@@ -8,12 +8,17 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
+import logging
 from dotenv import load_dotenv
 
 # dotenv_path = os.path.join("/home/ibrahim/bookkeeper-checklist/src", ".env")
 # load_dotenv(dotenv_path=dotenv_path)  # load all environment variables from .env.
 load_dotenv()
-
+# print(os.environ.get("STAGE_ENVIRONMENT"))
+log = logging.getLogger("my-logger")
+log.info("##########################")
+log.info(os.environ.get("STAGE_ENVIRONMENT"))
+log.info("##########################")
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bookkeeper_checklist.settings")

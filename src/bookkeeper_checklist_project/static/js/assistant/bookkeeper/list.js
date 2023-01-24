4f51b8dc2127882a5c69f7737764db9ebf632c31
Ibrahim Luqman <ibm.jnk@yandex.com>
2023-01-24 17:54:37 +0300

    feat: new views template files

---
+++ src/bookkeeper_checklist_project/static/js/assistant/bookkeeper/list.js
@@ -0,0 +1,12 @@
+"use strict";
+
+import { DataTableHelper } from "../../utils/datatable-helper.js";
+
+document.addEventListener("DOMContentLoaded", (ev) => {
+  /*
+    the browser fully loaded HTML, and the DOM tree is built, but external resources like pictures <img> and stylesheets may not yet have loaded.
+    */
+  const dataTableHelper = new DataTableHelper({
+    tableID: "assistantBookkeepersTable",
+  });
+});

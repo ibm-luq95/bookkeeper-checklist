c9d1f5b1c2fb8b149c1706157e24d7797c186d8a
Ibrahim Luqman <ibm.jnk@yandex.com>
2023-01-27 16:02:37 +0300

    feat: add jobs urls to assistant

--- src/bookkeeper_checklist_project/assistant/urls/__init__.py
+++ src/bookkeeper_checklist_project/assistant/urls/__init__.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path("dashboard/", include("assistant.urls.dashboard"), name="dashboard-urls"),
     path("bookkeepers/", include("assistant.urls.bookkeepers"), name="bookkeepers-urls"),
     path("client/", include("assistant.urls.client"), name="client-urls"),
+    path("job/", include("assistant.urls.job"), name="job-urls"),
     path(
         "important_contact/",
         include("assistant.urls.important_contact"),

5238991cd70790ba505c3adedfcf0e20d090a0b0
Ibrahim Luqman <ibm.jnk@yandex.com>
2023-01-27 15:57:29 +0300

    feat: create SaveCreatedByFormMixin

---
+++ src/bookkeeper_checklist_project/core/forms/created_by_form_mixin.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-#
+from typing import Any
+
+from django import forms
+
+
+class SaveCreatedByFormMixin(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    class Meta:
+        abstract = True
+
+    def save(self, commit=True) -> Any:
+        obj = super().save(commit=False)
+        if self.created_by:
+            obj.created_by = self.created_by
+        if commit:
+            obj.save()
+        return obj

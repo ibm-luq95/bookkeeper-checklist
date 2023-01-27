a9e01b546a05373bb09955a7a75730d2e63a1775
Ibrahim Luqman <ibm.jnk@yandex.com>
2023-01-27 15:58:59 +0300

    fix: extend CreatedBySerializerMixin to all serializers

--- src/bookkeeper_checklist_project/company_services/serializers/create_company_services.py
+++ src/bookkeeper_checklist_project/company_services/serializers/create_company_services.py
@@ -3,9 +3,12 @@ from rest_framework import serializers
 
 from company_services.helpers import PasswordHasher
 from company_services.models import CompanyService
+from core.serializers import CreatedBySerializerMixin
 
 
-class CreateCompanyServicesSerializer(serializers.ModelSerializer):
+class CreateCompanyServicesSerializer(
+    serializers.ModelSerializer, CreatedBySerializerMixin
+):
     class Meta:
         model = CompanyService
         exclude = (
--- src/bookkeeper_checklist_project/documents/serializers/create_document.py
+++ src/bookkeeper_checklist_project/documents/serializers/create_document.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-#
 from rest_framework import serializers
+
+from core.serializers import CreatedBySerializerMixin
 from documents.models import Documents
 
 
-class CreateDocumentSerializer(serializers.ModelSerializer):
+class CreateDocumentSerializer(serializers.ModelSerializer, CreatedBySerializerMixin):
     # document_file = serializers.FileField(use_url=True, allow_empty_file=False)
 
     class Meta:

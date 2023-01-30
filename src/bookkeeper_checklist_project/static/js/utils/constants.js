89888bddc132080a726e609847141e4876066a39
Ibrahim Luqman <ibm.jnk@yandex.com>
2023-01-30 16:13:39 +0300

    new: new js constants

--- src/bookkeeper_checklist_project/static/js/utils/constants.js
+++ src/bookkeeper_checklist_project/static/js/utils/constants.js
@@ -1,8 +1,27 @@
 "use strict";
 
+// init and create method to all objects, get object type
+Object.prototype.getConstructorName = function () {
+  const str = (
+    this.prototype ? this.prototype.constructor : this.constructor
+  ).toString();
+  const cname = str.match(/function\s(\w*)/)[1];
+  const aliases = ["", "anonymous", "Anonymous"];
+  return aliases.indexOf(cname) > -1 ? "Function" : cname;
+};
+
 const isDisabledCssClass = "is-disabled";
 const eyeSlashIconHTMLCode = `<i class="fa-solid fa-eye-slash"></i>`;
 const eyeIconHTMLCode = `<i class="fa-solid fa-eye"></i>`;
 const DEBUG = window.settings["DEBUG"];
+const FETCHURLNAMEURL = window.settings["FETCHURLNAMEURL"];
+const CURRENTUSER = window.settings["CURRENTUSER"];
 
-export { isDisabledCssClass, eyeSlashIconHTMLCode, eyeIconHTMLCode, DEBUG };
+export {
+  isDisabledCssClass,
+  eyeSlashIconHTMLCode,
+  eyeIconHTMLCode,
+  DEBUG,
+  FETCHURLNAMEURL,
+  CURRENTUSER,
+};

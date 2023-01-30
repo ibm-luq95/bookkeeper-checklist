e85ab620ca089ac01c1d64b96f7c6c2120e22a37
Ibrahim Luqman <ibm.jnk@yandex.com>
2023-01-30 16:02:49 +0300

    feat: import UpdateDocumentManagerView

--- src/bookkeeper_checklist_project/documents/views/__init__.py
+++ src/bookkeeper_checklist_project/documents/views/__init__.py
@@ -3,5 +3,6 @@ from .manager import (
     CreateManagerDocumentView,
     DetailsManagerDocumentView,
     ListManagerDocumentView,
+    UpdateDocumentManagerView,
 )
 from .bookkeeper import UpdateDocumentBookkeeperView

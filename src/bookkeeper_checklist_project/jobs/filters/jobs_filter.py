4486c5895bb71e212f8e430cc47c658ee6d9b680
Ibrahim Luqman <ibm.jnk@yandex.com>
2023-02-10 17:45:38 +0300

    feat: create jobs filter

---
+++ src/bookkeeper_checklist_project/jobs/filters/jobs_filter.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-#
+import django_filters
+from django import forms
+
+from jobs.models import Job
+from bookkeeper.models import get_all_bookkeepers_as_choices
+
+
+class JobFilter(django_filters.FilterSet):
+    due_date = django_filters.DateFilter(
+        field_name="due_date", widget=forms.DateInput(attrs={"type": "date"})
+    )
+    due_date__gt = django_filters.DateFilter(
+        field_name="due_date",
+        widget=forms.DateInput(attrs={"type": "date"}),
+        lookup_expr="gt",
+    )
+    due_date__lt = django_filters.DateFilter(
+        field_name="due_date",
+        widget=forms.DateInput(attrs={"type": "date"}),
+        lookup_expr="lt",
+    )
+    bookkeeper = django_filters.ChoiceFilter(
+        widget=forms.Select(), choices=get_all_bookkeepers_as_choices
+    )
+
+    class Meta:
+        model = Job
+        # form = JobFilterForm
+        fields = {
+            "title": ["icontains"],
+            # "bookkeeper": ["icontains"],
+            "client__name": ["icontains"],
+            "job_type": ["exact"],
+            "status": ["exact"],
+            # "due_date": ["gt", "lt"],
+        }
+        # fields = [
+        #     "title",
+        #     # "bookkeeper__user__first_name",
+        #     # "bookkeeper__user__last_name",
+        #     "bookkeeper__user",
+        #     "client__name",
+        #     "job_type",
+        #     "status",
+        #     "due_date",
+        # ]

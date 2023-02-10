cbc41800c68cc8f21f52cb28ff239e867e60e731
Ibrahim Luqman <ibm.jnk@yandex.com>
2023-02-10 17:30:45 +0300

    feat: add manager url

--- src/bookkeeper_checklist_project/jobs/urls/__init__.py
+++ src/bookkeeper_checklist_project/jobs/urls/__init__.py
@@ -5,4 +5,5 @@ app_name = "jobs"
 
 urlpatterns = [
     path("api/", include("jobs.urls.api"), name="jobs-api-urls"),
+    path("manager/", include("jobs.urls.manager"), name="jobs-manager-urls"),
 ]

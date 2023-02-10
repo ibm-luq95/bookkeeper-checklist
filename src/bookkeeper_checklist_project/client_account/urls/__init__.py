9ea53063e2e8a3845f0eae07369208310e18628a
Ibrahim Luqman <ibm.jnk@yandex.com>
2023-02-10 17:23:10 +0300

    feat: add manager url and views

--- src/bookkeeper_checklist_project/client_account/urls/__init__.py
+++ src/bookkeeper_checklist_project/client_account/urls/__init__.py
@@ -1,1 +1,19 @@
 # -*- coding: utf-8 -*-#
+from django.urls import path, include
+
+app_name = "accounts"
+
+urlpatterns = [
+    # path("api/", include("client.urls.api"), name="client-api-urls"),
+    path(
+        "manager/",
+        include("client_account.urls.manager"),
+        name="client-account-manager-urls",
+    ),
+    # path("assistant/", include("client.urls.assistant"), name="client-assistant-urls"),
+    # path(
+    #     "bookkeeper/",
+    #     include("client.urls.bookkeeper"),
+    #     name="client-bookkeeper-urls",
+    # ),
+]

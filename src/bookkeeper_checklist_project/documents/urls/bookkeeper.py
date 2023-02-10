39740b44bb5159015ce5080d6a6636615aee78b6
Ibrahim Luqman <ibm.jnk@yandex.com>
2023-02-10 17:40:34 +0300

    refact: refactoring views names

--- src/bookkeeper_checklist_project/documents/urls/bookkeeper.py
+++ src/bookkeeper_checklist_project/documents/urls/bookkeeper.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-#
 from django.urls import path
 
-from documents.views import UpdateDocumentBookkeeperView
+from documents.views import BookkeeperUpdateDocumentView
 
 app_name = "bookkeeper"
 
 urlpatterns = [
     # path("create/", CreateManagerDocumentView.as_view(), name="create"),
     # path("list/", ListManagerDocumentView.as_view(), name="list"),
-    path("update/<uuid:pk>", UpdateDocumentBookkeeperView.as_view(), name="update"),
+    path("update/<uuid:pk>", BookkeeperUpdateDocumentView.as_view(), name="update"),
 ]

d79ba6c593a0f469e9f00758f6902e7853c00271
Ibrahim Luqman <ibm.jnk@yandex.com>
2023-02-10 17:28:47 +0300

    fix: bug fixes
    Fix bug when update job due_date issue.
    add assistant input

--- src/bookkeeper_checklist_project/jobs/forms/jobs.py
+++ src/bookkeeper_checklist_project/jobs/forms/jobs.py
@@ -3,13 +3,22 @@ from django.core.exceptions import ValidationError
 from django.utils import timezone
 from django.utils.translation import gettext as _
 
+from assistant.models import Assistant
 from bookkeeper.models import Bookkeeper
 from core.forms import BaseModelFormMixin, SaveCreatedByFormMixin
 from jobs.models import Job
 
 
 class JobForm(BaseModelFormMixin, SaveCreatedByFormMixin):
-    def __init__(self, bookkeeper=None, client=None, created_by=None, *args, **kwargs):
+    def __init__(
+        self,
+        bookkeeper=None,
+        client=None,
+        created_by=None,
+        is_updated=False,
+        *args,
+        **kwargs
+    ):
         super(JobForm, self).__init__(*args, **kwargs)
         if bookkeeper is not None:
             self.fields["bookkeeper"].initial = bookkeeper
@@ -26,8 +35,15 @@ class JobForm(BaseModelFormMixin, SaveCreatedByFormMixin):
         if created_by is not None:
             self.created_by = created_by
 
+        self.is_update = is_updated
+
     bookkeeper = forms.ModelMultipleChoiceField(
         queryset=Bookkeeper.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False,
+    )
+    assistants = forms.ModelMultipleChoiceField(
+        queryset=Assistant.objects.all(),
         widget=forms.CheckboxSelectMultiple,
         required=False,
     )
@@ -36,8 +52,9 @@ class JobForm(BaseModelFormMixin, SaveCreatedByFormMixin):
         data = self.cleaned_data["due_date"]
         now = timezone.now().date()
 
-        if data < now:
-            raise ValidationError(_("Due date not valid!"), code="invalid")
+        if self.is_update is False:
+            if data < now:
+                raise ValidationError(_("Due date not valid!"), code="invalid")
 
         # Always return a value to use as the new cleaned data, even if
         # this method didn't change it.

76b5289e689b55a18965abc6f3b361e6e9db3360
Ibrahim Luqman <ibm.jnk@yandex.com>
2023-02-10 17:20:27 +0300

    feat: add new manager views and urls

--- src/bookkeeper_checklist_project/company_services/urls/__init__.py
+++ src/bookkeeper_checklist_project/company_services/urls/__init__.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-#
 from django.urls import path, include
 
-app_name = "company-services"
+app_name = "company_services"
 
 urlpatterns = [
     path("api/", include("company_services.urls.api"), name="company-services-api-urls"),
+    path(
+        "manager/",
+        include("company_services.urls.manager"),
+        name="company-services-manager-urls",
+    ),
 ]

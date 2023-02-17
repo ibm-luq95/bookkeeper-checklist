d71eb7c9146aa70011e87388ab647c07075e132d
Ibrahim Luqman <ibm.jnk@yandex.com>
2023-02-17 04:15:54 +0300

    feat: add is_show_created_at attribute

--- src/bookkeeper_checklist_project/core/views/mixins.py
+++ src/bookkeeper_checklist_project/core/views/mixins.py
@@ -9,6 +9,7 @@ class BaseListViewMixin:
         context = super().get_context_data(**kwargs)
         # debugging_print(dir(self.form_class))
         # debugging_print(self.form_class._meta.model._meta)
+        context.setdefault("is_show_created_at", True)
         if hasattr(self.model, "_meta"):
             context.setdefault("app_label", self.model._meta.app_label)
         return context

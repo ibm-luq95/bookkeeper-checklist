dbfe40a6a4f86fa06e203ad35865a4ccbb0ca72d
Ibrahim Luqman <ibm.jnk@yandex.com>
2023-02-18 22:31:14 +0300

    feat: import StartAndDueDateMixin

--- src/bookkeeper_checklist_project/core/models/__init__.py
+++ src/bookkeeper_checklist_project/core/models/__init__.py
@@ -1,7 +1,12 @@
 from .querysets import BaseQuerySetMixin
 from .managers import SoftDeleteManager
 from .mixins import BaseModelMixin
-from .mixins import UserForeignKeyMixin, CreatedByMixin, GetObjectSectionMixin
+from .mixins import (
+    UserForeignKeyMixin,
+    CreatedByMixin,
+    GetObjectSectionMixin,
+    StartAndDueDateMixin,
+)
 from .quote import Quote
 from .staff_member_mixin import StaffMemberMixin
 from .team_members_mixin import TeamMembersMixin

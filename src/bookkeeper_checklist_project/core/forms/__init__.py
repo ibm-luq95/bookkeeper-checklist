6b9657e1e1aaca45b66722c4fbe040b5c74cc0cb
Ibrahim Luqman <ibm.jnk@yandex.com>
2023-02-22 16:43:39 +0300

    feat: create new RemoveFieldsMixin mixin

--- src/bookkeeper_checklist_project/core/forms/__init__.py
+++ src/bookkeeper_checklist_project/core/forms/__init__.py
@@ -1,3 +1,4 @@
 from .base_model_form_mixin import BaseModelFormMixin
 from .html5_mixin import Html5Mixin
 from .created_by_form_mixin import SaveCreatedByFormMixin
+from .remove_fields_mixin import RemoveFieldsMixin

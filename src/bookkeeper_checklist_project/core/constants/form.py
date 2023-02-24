d2d458c3523a14caee0fe6fc319592316a08c543
Ibrahim Luqman <ibm.jnk@yandex.com>
2023-02-24 17:51:08 +0300

    fix: add updated_at

--- src/bookkeeper_checklist_project/core/constants/form.py
+++ src/bookkeeper_checklist_project/core/constants/form.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-#
 
-EXCLUDED_FIELDS = ["metadata", "is_deleted", "slug", "deleted_at"]
+EXCLUDED_FIELDS = ["metadata", "is_deleted", "slug", "deleted_at", "updated_at"]
 
 # Create any user form fields
 CREATE_USER_FORM_FIELDS = [

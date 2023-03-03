4261a8879250ceb80f037294429d80452310aa23
Ibrahim Luqman <ibm.jnk@yandex.com>
2023-03-03 21:18:20 +0300

    fix: register task template and task items to admin site

--- src/bookkeeper_checklist_project/task/admin.py
+++ src/bookkeeper_checklist_project/task/admin.py
@@ -1,10 +1,20 @@
 from core.admin import BaseAdminModelMixin
 from django.contrib import admin
 
-from .models import Task
+from .models import Task, TaskTemplate, TaskItem
 
 
 @admin.register(Task)
 class TaskAdmin(BaseAdminModelMixin):
     # pass
     list_filter = ("is_completed", "start_date", "due_date", "task_type")
+
+
+@admin.register(TaskTemplate)
+class TaskTemplateAdmin(BaseAdminModelMixin):
+    pass
+
+
+@admin.register(TaskItem)
+class TaskItemAdmin(BaseAdminModelMixin):
+    pass

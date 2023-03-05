7c3df0d9e4e76b5faab022cb5f7a318bc003383f
Ibrahim Luqman <ibm.jnk@yandex.com>
2023-03-05 20:38:26 +0300

    fix: use StrModelMixin

--- src/bookkeeper_checklist_project/client/models/category.py
+++ src/bookkeeper_checklist_project/client/models/category.py
@@ -2,8 +2,8 @@
 from django.db import models
 from django.utils.translation import gettext as _
 
-from core.models import BaseModelMixin
+from core.models import BaseModelMixin, StrModelMixin
 
 
-class ClientCategory(BaseModelMixin):
+class ClientCategory(BaseModelMixin, StrModelMixin):
     name = models.CharField(_("name"), max_length=50)

a00d04388ff158c42d2ccf63eafe6386f300bf75
Ibrahim Luqman <ibm.jnk@yandex.com>
2023-03-07 20:33:30 +0300

    fix: fix import order
    This order for import is fox circular import in other import in other models

--- src/bookkeeper_checklist_project/jobs/models/__init__.py
+++ src/bookkeeper_checklist_project/jobs/models/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-#
 from .category import JobCategory
 from .job import Job
-from .template import JobTemplate
 from .job_proxy import JobProxy
+from .template import JobTemplate

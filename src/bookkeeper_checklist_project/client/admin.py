dd25f008c93cadd6a8c9028d189a849fb4324138
Ibrahim Luqman <ibm.jnk@yandex.com>
2023-03-25 22:12:38 +0300

    fix: remove comment

--- src/bookkeeper_checklist_project/client/admin.py
+++ src/bookkeeper_checklist_project/client/admin.py
@@ -1,14 +1,8 @@
 from django.contrib import admin
 
-from bookkeeper.models import Bookkeeper
 from core.admin import BaseAdminModelMixin
 from core.constants import ADMIN_FILTER_LIST
 from .models import Client, ClientCategory
-
-
-# class BookkeeperInline(admin.TabularInline):
-#     model = Bookkeeper.clients.through
-#     extra = 2
 
 
 @admin.register(Client)

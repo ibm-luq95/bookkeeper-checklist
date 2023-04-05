c6ab96240bf7e89e0c1d492c80f7d5727bc54b51
Ibrahim Luqman <ibm.jnk@yandex.com>
2023-04-05 23:41:02 +0300

    feat: create random method for all models managers

--- src/bookkeeper_checklist_project/core/models/managers.py
+++ src/bookkeeper_checklist_project/core/models/managers.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-#
+from django.db.models.aggregates import Count
+from random import randint
 from django.db import models
 from django.db.models import Q
 
@@ -13,3 +15,8 @@ class SoftDeleteManager(models.Manager):
         )
 
         return queryset
+
+    def random(self):
+        count = self.aggregate(count=Count("id"))["count"]
+        random_index = randint(0, count - 1)
+        return self.all()[random_index]

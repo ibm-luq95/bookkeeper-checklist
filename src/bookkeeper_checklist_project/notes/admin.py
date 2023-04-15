3c8285e38bce4a23ffa36cbedda8b64a3627feba
Ibrahim Luqman <ibm.jnk@yandex.com>
2023-04-15 23:49:17 +0300

    feat: use new admin mixin SummernoteAdminMixin

--- src/bookkeeper_checklist_project/notes/admin.py
+++ src/bookkeeper_checklist_project/notes/admin.py
@@ -1,14 +1,16 @@
-from core.admin import BaseAdminModelMixin
+# from django_summernote.admin import SummernoteModelAdmin
+
+from core.admin import BaseAdminModelMixin, SummernoteAdminMixin
 from django.contrib import admin
 
 from .models import Note, NoteTemplate
 
 
 @admin.register(Note)
-class NoteAdmin(BaseAdminModelMixin):
+class NoteAdmin(BaseAdminModelMixin, SummernoteAdminMixin):
     pass
 
 
 @admin.register(NoteTemplate)
-class NoteTemplateAdmin(BaseAdminModelMixin):
+class NoteTemplateAdmin(BaseAdminModelMixin, SummernoteAdminMixin):
     pass

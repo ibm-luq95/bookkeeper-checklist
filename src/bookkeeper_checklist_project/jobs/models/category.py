8f7d5a104395a4c2a0e317e90a9240eea3724025
Ibrahim Luqman <ibm.jnk@yandex.com>
2023-05-03 15:35:51 +0300

    fix: add constraints

--- src/bookkeeper_checklist_project/jobs/models/category.py
+++ src/bookkeeper_checklist_project/jobs/models/category.py
@@ -7,3 +7,8 @@ from core.models import BaseModelMixin, CreatedByMixin, StrModelMixin
 
 class JobCategory(BaseModelMixin, StrModelMixin):
     name = models.CharField(_("name"), max_length=50, unique=True, db_index=True)
+
+    class Meta(BaseModelMixin.Meta):
+        constraints = [
+            models.UniqueConstraint(fields=["name"], name="unique_job_category_name")
+        ]

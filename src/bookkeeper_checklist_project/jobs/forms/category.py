3443bfb60a5381a614dbf1d488b4b5cc7fcc1104
Ibrahim Luqman <ibm.jnk@yandex.com>
2023-05-03 15:35:22 +0300

    fix: fix duplicate entry issue

--- src/bookkeeper_checklist_project/jobs/forms/category.py
+++ src/bookkeeper_checklist_project/jobs/forms/category.py
@@ -1,3 +1,8 @@
+from typing import Any
+
+from django.forms import ValidationError
+from django.utils.translation import gettext as _
+
 from core.forms import (
     BaseModelFormMixin,
 )
@@ -7,6 +12,16 @@ from jobs.models import JobCategory
 class JobCategoryForm(BaseModelFormMixin):
     def __int__(self, *args, **kwargs):
         super(JobCategoryForm, self).__init__(*args, **kwargs)
+
+    def clean(self) -> dict[str, Any] | None:
+        cleaned_data = super().clean()
+        check = JobCategory.original_objects.filter(name=cleaned_data.get("name"))
+        if check.exists():
+            # self.add_error("name", cleaned_data.get('name'))
+            raise ValidationError(
+                _(f"The name: {cleaned_data.get('name')} exists!"), code="invalid"
+            )
+        return cleaned_data
 
     class Meta:
         model = JobCategory
